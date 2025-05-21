36a46971f96d4aea5f15cfd1c3cd9dafb7d49b4b
johna-lee <jltrooper@gmail.com>
2025-05-20 19:04:45 -0500

    scrapes URLs for each competition's match reports

--- scrape_urls.py
+++ scrape_urls.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 import random
 
-# Set up headers to mimic a browser
+# Set up headers to mimic a web browser
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
